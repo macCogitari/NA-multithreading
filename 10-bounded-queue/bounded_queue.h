fe41bab2d65146bbedc63873c3fbb9e42631af97
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-30 15:43:33 +0100

    init

--- 10-bounded-queue/bounded_queue.h
+++ 10-bounded-queue/bounded_queue.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <array>
+#include <mutex>
 
 template<typename T, std::size_t N>
 class bounded_queue
@@ -8,4 +10,11 @@ class bounded_queue
 public:
     void push_back(T t);
     T pop_front();
+private:
+    std::mutex _mtx;
+
+    std::array<T,N> _buffer;
+    std::size_t _front = 0;
+    std::size_t _back = 0;
+    std::size_t _size = 0;
 };
