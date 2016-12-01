bac49e2e0b012a2cf77e7f8b548408053a16700c
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-12-01 12:20:58 +0100

    spinlock implementation

--- 20-semaphore/semaphore.h
+++ 20-semaphore/semaphore.h
@@ -1,13 +1,55 @@
 #pragma once
 
 #include <cstddef>
+//#include <atomic>
+#include <condition_variable>
+#include <mutex>
 
 class semaphore
 {
 public:
-    semaphore(std::size_t count);
-    void wait();
-    bool try_wait();
-    void signal(std::size_t count = 1);
+    semaphore(std::size_t count = 0)
+        : _count{count}
+    {
+    }
+
+    void wait()
+    {
+        std::unique_lock<std::mutex> lock{_mtx};
+        _cv.wait(lock, [&](){return _count != 0;});
+        --_count;
+    }
+
+    bool try_wait()
+    {
+        std::unique_lock<std::mutex> lock{_mtx};
+        if(_count > 0){
+            _count--;
+            return true;
+        }
+        return false;
+    }
+
+    void signal(std::size_t count = 1)
+    {
+        std::unique_lock<std::mutex> lock{_mtx};
+//        auto old = +_count; //decay czyli tę samą wartość ale kopia
+//        _count += count;
+//        if(old){
+//            return;
+//        }
+        while(count--)
+        {
+            _cv.notify_one();
+        }
+
+    }
+
+private:
+    std::mutex _mtx;
+    std::condition_variable _cv;
+
+//    std::atomic<std::size_t> _count;
+    std::size_t _count;
 };
 
--- 30-spinlock/spinlock.h
+++ 30-spinlock/spinlock.h
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <atomic>
+
 class spinlock
 {
 public:
-    void lock();
-    bool try_lock();
-    void unlock();
+    void lock()
+    {
+        while(!try_lock())
+        {
+        }
+    }
+
+    bool try_lock()
+    {
+        return !_locked.exchange(true);
+    }
+
+    void unlock()
+    {
+        assert(_locked); // błąd pre condition
+                        // nie wolno wywołać dwa razy unlock
+        _locked = false;
+    }
+
+private:
+    std::atomic<bool> _locked{false};
 };
 
